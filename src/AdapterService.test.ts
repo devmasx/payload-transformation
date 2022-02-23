a6971a94305bf27b0499ccfd12f5d4c0b15a8328
Miguel Savignano <migue.masx@gmail.com>
2022-02-24 00:43:17 +0100

    jest test

---
+++ src/AdapterService.test.ts
@@ -0,0 +1,37 @@
+const YAML = require('yaml');
+const fs = require('fs');
+const user = require('../examples/user.json');
+import { AdapterService } from './AdapterService';
+
+const functions = {
+  fetchRoles(context: any) {
+    return context.email == 'jhonDoe@gmail.com' ? 'admin' : 'user';
+  },
+};
+
+const processors = {
+  $to_cents: function (contextValue) {
+    return contextValue * 1000;
+  },
+};
+
+describe('AdapterService', () => {
+  it('#run', () => {
+    const text = fs.readFileSync(`./examples/user_adapter.yaml`, 'utf8');
+    const config = YAML.parse(text);
+    const result = new AdapterService(config, user, {
+      functions,
+      processors,
+    }).run();
+    expect(result).toEqual({
+      avatarUrl: 'https://avatar.com/jhon',
+      billingAddress: 'addresses.billing',
+      budget_in_cents: 100000,
+      fullname: 'Jhon Doe',
+      id: 'a190f1a6-2fb8-77d9-fbb9-9b8a19e441e3',
+      name: 'Jhon',
+      provider: 'google',
+      roles: 'admin',
+    });
+  });
+});
