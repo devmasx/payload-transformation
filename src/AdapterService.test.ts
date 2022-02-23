185e7f805b25520b359690a7bd691ded3f51daaa
Miguel Savignano <migue.masx@gmail.com>
2022-02-24 00:50:08 +0100

    test missing functions

--- src/AdapterService.test.ts
+++ src/AdapterService.test.ts
@@ -14,24 +14,36 @@ const processors = {
     return contextValue * 1000;
   },
 };
+const text = fs.readFileSync(`./examples/user_adapter.yaml`, 'utf8');
+const config = YAML.parse(text);
 
 describe('AdapterService', () => {
-  it('#run', () => {
-    const text = fs.readFileSync(`./examples/user_adapter.yaml`, 'utf8');
-    const config = YAML.parse(text);
-    const result = new AdapterService(config, user, {
-      functions,
-      processors,
-    }).run();
-    expect(result).toEqual({
-      avatarUrl: 'https://avatar.com/jhon',
-      billingAddress: 'addresses.billing',
-      budget_in_cents: 100000,
-      fullname: 'Jhon Doe',
-      id: 'a190f1a6-2fb8-77d9-fbb9-9b8a19e441e3',
-      name: 'Jhon',
-      provider: 'google',
-      roles: 'admin',
+  describe('#run', () => {
+    it('success', () => {
+      const result = new AdapterService(config, user, {
+        functions,
+        processors,
+      }).run();
+      expect(result).toEqual({
+        avatarUrl: 'https://avatar.com/jhon',
+        billingAddress: 'addresses.billing',
+        budget_in_cents: 100000,
+        fullname: 'Jhon Doe',
+        id: 'a190f1a6-2fb8-77d9-fbb9-9b8a19e441e3',
+        name: 'Jhon',
+        provider: 'google',
+        roles: 'admin',
+      });
+    });
+
+    it('not found function', () => {
+      try {
+        new AdapterService(config, user, {
+          processors,
+        }).run();
+      } catch (e) {
+        expect(e.message).toBe('Function fetchRoles is not defined');
+      }
     });
   });
 });
